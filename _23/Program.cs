9c330e16db8914ea684031adc3d25708ef438214
Kirill Miller <belogorski2000@gmail.com>
2022-08-26 11:42:48 +0300

    Добавьте файлы проекта.

---
+++ _23/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Разработать асинхронный метод для 
+вычисления факториала числа. В методе 
+Main выполнить проверку работы метода.*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FakAsync(n);
+
+            Console.ReadKey();  
+        }
+
+        static void Fak(int n)
+        {
+            int s = 1;
+            for (int i = 1; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+        }
+        static async void FakAsync(int n)
+        {
+            await Task.Run(() => Fak(n));
+        }
+    }
+}
